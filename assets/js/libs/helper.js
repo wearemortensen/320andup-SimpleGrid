eb78e420cccb472a899765212b487c6498e00f1c
Rude <rude@mortensen.co>
2012-02-13 11:54:43 +0100

    Added iOS orientation bug fix from https://github.com/scottjehl/iOS-Orientationchange-Fix

--- assets/js/libs/helper.js
+++ assets/js/libs/helper.js
@@ -10,6 +10,62 @@ c=b(this).find("a:first-child").attr("href");a+=b(this).clone().children("ul, ol
 f(a)&&(a.show(),b("#mobileMenu_"+a.attr("id")).hide())}var d={switchWidth:768,topOptionText:"Select a page",indentString:"&nbsp;&nbsp;&nbsp;"};return this.each(function(){g&&b.extend(d,g);var a=b(this);b(window).resize(function(){j(a)});j(a)})}})(jQuery);
 
 /*
+ A fix for the dreaded iOS orientationchange zoom bug http://adactio.com/journal/5088/. Seems to work!
+ Authored by @scottjehl. Props to @wilto for addressing a tilt caveat.
+ MIT License.
+
+FOR LATEST, SEE https://github.com/scottjehl/iOS-Orientationchange-Fix
+*/
+
+(function(w){
+    var doc = w.document;
+
+    if( !doc.querySelectorAll ){ return; }
+
+    var meta = doc.querySelectorAll( "meta[name=viewport]" )[ 0 ],
+        initialContent = meta && meta.getAttribute( "content" ),
+        disabledZoom = initialContent + ", maximum-scale=1.0",
+        enabledZoom = initialContent + ", maximum-scale=10.0",
+        enabled = true,
+        orientation = w.orientation,
+        rotation = 0;
+
+    if( !meta ){ return; }
+
+    function restoreZoom(){
+        meta.setAttribute( "content", enabledZoom );
+        document.body.innerHTML = document.body.innerHTML;
+        enabled = true;
+    }
+
+    function disableZoom(){
+        meta.setAttribute( "content", disabledZoom );
+        enabled = false;
+    }
+
+    function checkTilt( e ){
+        orientation = Math.abs( w.orientation );
+        rotation = Math.abs( e.gamma );
+
+        if( rotation > 8 && orientation === 0 ){
+            if( enabled ){
+                disableZoom();
+            }   
+        }
+        else {
+            if( !enabled ){
+                restoreZoom();
+            }
+        }
+
+    }
+
+    w.addEventListener( "orientationchange", restoreZoom, false );
+    w.addEventListener( "deviceorientation", checkTilt, false );
+
+})( this );
+
+/*
  * MBP - Mobile boilerplate helper functions
  */
 (function(document){
